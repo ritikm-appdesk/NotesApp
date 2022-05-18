6ff8f917556f7c76b44d44c21f3991cc3ebac9fd
ritikm-appdesk <105192214+ritikm-appdesk@users.noreply.github.com>
2022-05-18 17:25:54 +0530

    Complete Build

--- app/src/main/java/com/example/notesapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/ui/activities/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.notesapp.R
 import com.example.notesapp.viewmodel.NotesViewModel
 
-
 class MainActivity : AppCompatActivity() {
     private val viewModel: NotesViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
